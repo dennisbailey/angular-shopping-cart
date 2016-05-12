d4b6857135697a19e20c70f8c75fca8d21f6e062
Dennis Bailey <bailey.dennis@gmail.com>
2016-05-12 15:28:42 -0600

    ...

--- js/router.js
+++ js/router.js
@@ -1,6 +1,6 @@
-app.config(function($routeProvider, 
-// $locationProvider
-) {
+app.config(function($routeProvider) 
+// $locationProvider) 
+{
     $routeProvider
       .when('/', {
         templateUrl: '../partials/main.html',
